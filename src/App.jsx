26e73350dd5e7e88578f9c4038ae87da25f338f9
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2025-07-10 01:35:27 +0300

    remove MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { CssBaseline } from "@mui/material";
+import ResponsiveAppBar from "./components/NavBar/NavBar";
 
 function App() {
-  return <>
-  
-  </>
+  return (
+    <>
+
+    </>
+  );
 }
 
-export default App
+export default App;
