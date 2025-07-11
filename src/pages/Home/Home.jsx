1f3fe2c7604fa607527d3ed0270e1e2ce43516db
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2025-07-11 05:22:54 +0300

    SuccessStories /Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,16 @@ import Features from "./Features";
 import Hero from "./Hero";
 import HowItWorks from "./HowItWorks";
 import OurMentors from "./OurMentors";
+import SuccessStories from "./SuccessStories";
 
 function Home() {
   return (
     <div>
       <Hero />
       <Features />
-      <OurMentors/>
-      <HowItWorks/>
+      <OurMentors />
+      <HowItWorks />
+      <SuccessStories />
     </div>
   );
 }
