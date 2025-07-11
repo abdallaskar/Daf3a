9125eefc69fc312c608fbc455b07a946c6c0991f
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2025-07-11 05:07:27 +0300

    OurMentors /Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Features from "./Features";
 import Hero from "./Hero";
+import OurMentors from "./OurMentors";
 
 function Home() {
   return (
     <div>
       <Hero />
       <Features />
+      <OurMentors/>
     </div>
   );
 }
