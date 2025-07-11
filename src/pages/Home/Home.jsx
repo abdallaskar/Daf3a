04afc4c87f22b1c2f5ce5238bfd489e512414abe
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2025-07-11 05:15:35 +0300

    HowItWorks /Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Features from "./Features";
 import Hero from "./Hero";
+import HowItWorks from "./HowItWorks";
 import OurMentors from "./OurMentors";
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
       <Hero />
       <Features />
       <OurMentors/>
+      <HowItWorks/>
     </div>
   );
 }
