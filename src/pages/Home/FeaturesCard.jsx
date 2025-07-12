a1d25d702687b5f1cf8d69c37319c5a75cf97355
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2025-07-12 17:28:10 +0300

    fix Home page Success stories font color

--- src/pages/Home/FeaturesCard.jsx
+++ src/pages/Home/FeaturesCard.jsx
@@ -5,7 +5,7 @@ function FeaturesCard({ icon, title, description }) {
         <div className="mb-6 flex h-16 w-16 items-center justify-center rounded-full icon-container ">
           {icon}
         </div>
-        <h3 className="font-poppins text-xl font-semibold">{title}</h3>
+        <h3 className="font-poppins text-xl text-primary font-semibold">{title}</h3>
         <p className="mt-2 text-base text-secondary">{description}</p>
       </div>
     </>
