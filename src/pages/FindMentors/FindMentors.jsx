382bf295f7aebdb9b53fbd3c2f6f7fe5517c47db
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2025-07-15 02:59:33 +0300

    FindMentors Header UI

--- src/pages/FindMentors/FindMentors.jsx
+++ src/pages/FindMentors/FindMentors.jsx
@@ -1,9 +1,23 @@
+
+import FindMentorsHeader from "./FindMentorsHeader";
+import FindMentorsInput from "./FindMentorsInput";
+
 function FindMentors() {
-    return (
-        <div>
+  return (
+    <>
+      <div className="relative bg-background flex size-full min-h-screen flex-col overflow-x-hidden">
+        <div className="layout-container flex h-full grow flex-col">
+          <main className="flex-1 container mx-auto px-6 py-10">
+            <div className="flex flex-col gap-8">
+              <FindMentorsHeader />
+              <FindMentorsInput />
             
+            </div>
+          </main>
         </div>
-    )
+      </div>
+    </>
+  );
 }
 
-export default FindMentors
+export default FindMentors;
---
+++ src/pages/FindMentors/FindMentorsHeader.jsx
@@ -0,0 +1,20 @@
+function FindMentorsHeader() {
+  return (
+    <div className="flex flex-col mt-15 gap-4 text-center">
+      <h2 className="text-3xl font-bold font-poppins text-primary">
+        Find Your Perfect Mentor
+      </h2>
+      <p className="text-secondary max-w-2xl mx-auto">
+        Based on your interests in
+        <span className="font-semibold text-brand mx-1">
+          Product Management
+        </span>
+        and
+        <span className="font-semibold text-brand mx-1">UX Design</span>, we've
+        found these mentors for you.
+      </p>
+    </div>
+  );
+}
+
+export default FindMentorsHeader;
