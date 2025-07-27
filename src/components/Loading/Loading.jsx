c6f6dd7e8ae79c6d55a2bfe8322025cf07781a06
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2025-07-28 00:21:41 +0300

    Handle loading Dark mode

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,7 +1,7 @@
 import { ClipLoader } from "react-spinners";
 function Loading() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center bg-background justify-center h-screen">
       <ClipLoader color="#2e7d6b" size={40} speedMultiplier={1} />
     </div>
   );
